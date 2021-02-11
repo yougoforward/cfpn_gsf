e11f4b326d91c14abdde06b976e9707bf8e2d51d
yougoforward <908865817@qq.com>
2021-02-11 17:07:48 +0800

    3

--- encoding/models/cfpn_gsf2.py
+++ encoding/models/cfpn_gsf2.py
@@ -126,6 +126,9 @@ class localUp(nn.Module):
         self.refine = nn.Sequential(nn.Conv2d(out_channels, out_channels//2, 3, padding=2, dilation=2, bias=False),
                                    norm_layer(out_channels//2),
                                    nn.ReLU(),
+                                   nn.Conv2d(out_channels//2, out_channels//2, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(out_channels//2),
+                                   nn.ReLU(),
                                     )
         self.project2 = nn.Sequential(nn.Conv2d(out_channels//2, out_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(out_channels),
