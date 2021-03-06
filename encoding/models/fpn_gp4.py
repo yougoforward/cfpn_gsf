b6a3d1fb03abba3f7d8a4743c099f6e5a675d80e
yougoforward <908865817@qq.com>
2021-03-06 22:12:56 +0800

    fpn_gp2

---
+++ encoding/models/fpn_gp4.py
@@ -0,0 +1,97 @@
+from __future__ import division
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .fcn import FCNHead
+from .base import BaseNet
+
+__all__ = ['fpn_gp4', 'get_fpn_gp4']
+
+
+class fpn_gp4(BaseNet):
+    def __init__(self, nclass, backbone, aux=True, se_loss=False, norm_layer=nn.BatchNorm2d, **kwargs):
+        super(fpn_gp4, self).__init__(nclass, backbone, aux, se_loss, norm_layer=norm_layer, **kwargs)
+
+        self.head = fpn_gp4Head(2048, nclass, norm_layer, se_loss, jpu=kwargs['jpu'], up_kwargs=self._up_kwargs)
+        if aux:
+            self.auxlayer = FCNHead(1024, nclass, norm_layer)
+
+    def forward(self, x):
+        imsize = x.size()[2:]
+        c1, c2, c3, c4 = self.base_forward(x)
+        x = self.head(c1,c2,c3,c4)
+        x = F.interpolate(x, imsize, **self._up_kwargs)
+        outputs = [x]
+        if self.aux:
+            auxout = self.auxlayer(c3)
+            auxout = F.interpolate(auxout, imsize, **self._up_kwargs)
+            outputs.append(auxout)
+        return tuple(outputs)
+
+
+
+class fpn_gp4Head(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer, se_loss, jpu=False, up_kwargs=None,
+                 atrous_rates=(12, 24, 36)):
+        super(fpn_gp4Head, self).__init__()
+        self.se_loss = se_loss
+        self._up_kwargs = up_kwargs
+
+        inter_channels = in_channels // 4
+        self.conv5 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU(),
+                                   )
+
+        self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
+
+        self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
+        self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
+        self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                            nn.Conv2d(inter_channels, inter_channels, 1, bias=False),
+                            norm_layer(inter_channels),
+                            nn.ReLU(True))
+
+    def forward(self, c1,c2,c3,c4):
+        _,_, h,w = c2.size()
+        out = self.conv5(c4)
+               
+        out3 = self.localUp4(c3, out)  
+        out = self.localUp3(c2, out3)
+        #gp
+        gp = self.gap(out)    
+        #
+        out = torch.cat([out, gp.expand_as(out)], dim=1)
+        
+        return self.conv6(out)
+
+class localUp(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
+        super(localUp, self).__init__()
+        self.connect = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels),
+                                   nn.ReLU())
+
+        self._up_kwargs = up_kwargs
+
+    def forward(self, c1,c2):
+        n,c,h,w =c1.size()
+        c1p = self.connect(c1) # n, 64, h, w
+        c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
+        out = c1p + c2
+        return out
+
+
+def get_fpn_gp4(dataset='pascal_voc', backbone='resnet50', pretrained=False,
+                 root='~/.encoding/models', **kwargs):
+    # infer number of classes
+    from ..datasets import datasets
+    model = fpn_gp4(datasets[dataset.lower()].NUM_CLASS, backbone=backbone, root=root, **kwargs)
+    if pretrained:
+        raise NotImplementedError
+
+    return model
+
+
